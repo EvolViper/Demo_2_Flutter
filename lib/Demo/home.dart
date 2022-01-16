cd16f575b968e04ff5ba9983f0e7f04415f0f941
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-16 21:44:09 +0700

    Add home page

--- lib/Demo/home.dart
+++ lib/Demo/home.dart
@@ -1,16 +1,103 @@
 import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget{
+import 'model/product.dart';
+import 'model/products_repository.dart';
+import 'package:intl/intl.dart';
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('I make it!!!'),
+      appBar: AppBar(
+        title: const Text('Fish'),
+        backgroundColor: Colors.purple,
+        leading: IconButton(
+            onPressed: () {
+              print('Menu Here');
+            },
+            icon: const Icon(Icons.menu)),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              print('Search Button');
+            },
+            icon: const Icon(Icons.search, semanticLabel: 'search'),
+          ),
+          IconButton(
+              onPressed: () {
+                print('Filter button');
+              },
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              )),
+        ],
+      ),
+      // Add a gid view
+      body: GridView.count(
+        crossAxisCount:
+            2, // chỉ định số lượng các mặt hàng sẽ xuất hiện trên app
+        padding: const EdgeInsets.all(16.0),
+        // calculates each child's width ([width of entire grid] - [left padding] - [right padding]) / 2
+        // => ([width of entire grid] - 16 - 16 / 2)
+        childAspectRatio: 8.0 / 9.0, // khai báo kích cỡ khung của các mặt hàng
+        // Build a grid of card
+        children: _buildGridCards(context), // call _buildGridCards function to get list of card
       ),
       resizeToAvoidBottomInset: false,
     );
   }
 
+  // remember that functions starting with an underscore are private API
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductRepository.loadProducts(Category.all);
+
+    if(products.isEmpty){
+      return const<Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product){
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        color: Colors.purple,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            AspectRatio(
+                aspectRatio: 18 / 11,
+              child:  Image.asset(
+                product.assetName,
+                fit: BoxFit.fill,
+                // package: product.assetPackage,
+              ),
+            ),
+            Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: theme.textTheme.headline6,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        formatter.format(product.price),
+                        style: theme.textTheme.subtitle2,
+                      )
+                    ],
+                  ),
+                ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
 }
